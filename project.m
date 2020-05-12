eaae15a8f981c84eda34e2b71a4043021b8c16bb
Otto Holmström <ottome@live.com>
2020-05-12 16:48:45 +0200

    work done 12/5, working(?) stationary solution

--- project.m
+++ project.m
@@ -13,12 +13,17 @@ coord=p' ;
 ndof=max(max(t(1:3,:)));
 [Ex,Ey]=coordxtr(edof,coord,(1:ndof)',3);
 
+elength = length(e);
+
 %eldraw2(Ex,Ey,[1,4,1]);
 %hold on
 %eldraw2(-Ex,Ey,[1,4,1]);
 
-
+%-Material parameters-%
 Q = 5 * 10^(7);
+T0 = 30;
+Tinf = 18;
+ac = 40;
 ep = 0.01;
 
 kAg = 5;
@@ -26,29 +31,52 @@ kSi = 149;
 kCu = 385;
 
 K = zeros(ndof);
-f = zeros(ndof, 1);
+fl = zeros(ndof, 1);
+fb = zeros(ndof, 1);
 
-for elnbr = 1:nelm
-    zone = t(4,elnbr);
-    k = kCu;
-    if zone == 2
-        k = kSi;
+
+ereduced = e([1 2 5],:);
+conv_segments = [8 20 21]; %Boundary segments with convection
+
+qn_segments = [1 18 19 23 24];
+
+bc = [];
+flow_elements = [];
+
+for i  =1:elength
+    if ismember(ereduced(3,i),conv_segments)
+        bc = [bc
+              ereduced(1,i) Tinf];
     end
-    if zone == 1
-        k = kAg;
-    end
-    
-    index = edof(elnbr,2:end);
-        
-    Ke = flw2te(Ex(elnbr,:),Ey(elnbr,:),ep, k*ones(2));
-    
-    fe = 0;
-    
-    K(index,index) = K(index,index) + Ke; 
-    f(index) = f(index) + fe;
 end
 
-a = solveq(K,f);
+
+
+%-Solve-%
+for elnbr = 1:nelm
+    zone = t(4,elnbr);
+    if zone == 3
+        k = kSi;
+        nodes = t(1:3, elnbr);
+        xy = coord(nodes,:);
+        area = polyarea(xy(:,1),xy(:,2))*10^(-6);
+        fl(nodes) =  Q*area*ep;
+    elseif zone == 1
+        k = kAg;
+    else
+        k = kCu;
+    end
+        
+    Ke = flw2te(Ex(elnbr,:)./1000,Ey(elnbr,:)./1000,ep, k*eye(2));
+    index = edof(elnbr,2:end);
+    K(index,index) = K(index,index) + Ke;
+end
+f = fl + fb;
+
+a = solveq(K,f,bc);
 ed = extract(edof, a);
 patch(Ex',Ey',ed');
+hold on
+patch(-Ex',Ey',ed');
+colormap hot
 colorbar
